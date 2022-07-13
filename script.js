98165c0de307287e8b16b2b959f1c249deda5ee2
Alan Foster <alan-af@outlook.com>
2022-07-13 19:46:11 -0300

    tentando fazer o avaliador identificar o id form-data

--- script.js
+++ script.js
@@ -27,3 +27,39 @@ agreeCheckbox.addEventListener('click', (event) => {
 textArea.onkeyup = () => {
   counter.innerHTML = `${500 - textArea.value.length}`;
 };
+
+function getSelectedCheckboxes(query) {
+  const selectedContent = [];
+  selectedContent.id = 'form-data';
+
+  const checkboxes = document.querySelectorAll(query);
+
+  for (let index = 0; index < checkboxes.length; index += 1) {
+    const checkbox = checkboxes[index];
+    if (checkbox.checked) {
+      selectedContent.push(checkbox.value);
+    }
+  }
+
+  return selectedContent;
+}
+
+function setupFormSubmit() {
+  const form = document.querySelector('#evaluation-form');
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#input-name').value;
+    const lastname = document.querySelector('#input-lastname').value;
+    const email = document.querySelector('#input-email').value;
+    const house = document.querySelector('#house').value;
+    const family = document.querySelector('input[type="radio"][name="family"]:checked').value;
+    const checkboxes = getSelectedCheckboxes('input[type="checkbox"][name="content"]');
+    const rate = document.querySelector('input[type="radio"][name="rate"]:checked').value;
+    const observacoes = document.querySelector('#textarea').value;
+    form.innerHTML = `Nome: ${name} ${lastname} Email: ${email} Casa: ${house} Família: ${family}`;
+    form.innerHTML += `Matérias: ${checkboxes.join(', ')} Avaliação: ${rate}`;
+    form.innerHTML += `Observações: ${observacoes}`;
+  };
+}
+setupFormSubmit();
